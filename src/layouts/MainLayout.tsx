f4d21fd777392b1dc5d922f6ede9a655116e6b05
Dev Lanageuse <dev@lanageuse.com>
2025-09-02 14:32:10 +0200

    feat : creation du composant MainLayout

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+
+interface MainLayoutProps {
+  children: ReactNode;
+}
+
+const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+  return (
+    <main>
+      {children}
+    </main>
+  )
+}
+  export default MainLayout
