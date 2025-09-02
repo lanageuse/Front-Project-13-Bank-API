668f0ff63625c7d73531d61e941d6efffc5e0af4
Dev Lanageuse <dev@lanageuse.com>
2025-09-02 17:00:50 +0200

    feat: conversion de NavBar pour utiliser la navigation React Router
    - Remplacement des balises anchor par des composants Link pour le routage côté client
    - Mise à jour de l'import pour inclure Link depuis react-router
    - Changement de l'export du composant de nommé vers export par défaut
    - Ajout des chemins de routage pour l'accueil (/) et les pages de connexion

--- src/UI/navBar/NavBar.tsx
+++ src/UI/navBar/NavBar.tsx
@@ -1,24 +1,27 @@
-import argentBankLogo from '../../img/argentBankLogo.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import argentBankLogo from "../../img/argentBankLogo.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons"
+import { Link } from "react-router"
 
-export const Navigation = () => {
-  return(
+const NavBar = () => {
+  return (
     <nav className="main-nav">
-      <a className="main-nav-logo" href="./">
+        <Link to={{pathname : '/'}} className="main-nav-logo" >
         <img
           className="main-nav-logo-image"
           src={argentBankLogo}
           alt="Argent Bank Logo"
         />
         <h1 className="sr-only">Argent Bank</h1>
-      </a>
+      </Link>
       <div>
-        <a className="main-nav-item" href="./sign-in.html">
+        <Link to={{pathname : 'login'}} className="main-nav-item">
           <FontAwesomeIcon icon={faUserCircle} />
           Sign In
-        </a>
+        </Link>
       </div>
     </nav>
   )
 }
+
+export default NavBar
