76eccf093793128ad9ea8897dea9e1ca2c6a15c8
Dev Lanageuse <dev@lanageuse.com>
2025-09-02 14:29:02 +0200

    feat : ajout du composant Hero avec image de fond et contenu promotionnel

---
+++ src/UI/hero/Hero.tsx
@@ -0,0 +1,16 @@
+import bgHero  from '../../img/bank-tree.jpeg'
+const Hero = () => {
+  return (
+    <div className="hero" style={{backgroundImage : `url(${bgHero})`}}>
+      <section className="hero-content">
+        <h2 className="sr-only">Promoted Content</h2>
+        <p className="subtitle">No fees.</p>
+        <p className="subtitle">No minimum deposit.</p>
+        <p className="subtitle">High interest rates.</p>
+        <p className="text">Open a savings account with Argent Bank today!</p>
+      </section>
+    </div>
+  )
+}
+
+export default Hero
