c1273cb3b80dea13f0810f5764395513966677ce
Dev Lanageuse <dev@lanageuse.com>
2025-09-03 09:08:15 +0200

    Renommage du composant Login en SignIn et MAj des imports

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
+import Login from "./pages/SignIn";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
 
