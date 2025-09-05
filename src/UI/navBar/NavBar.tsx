e8b0be5344f365eb46f59c61be6d7df7cffdaa30
Dev Lanageuse <dev@lanageuse.com>
2025-09-06 01:54:12 +0200

    feat: Ajout du lien vers le profile user et MAj des icons

--- src/UI/navBar/NavBar.tsx
+++ src/UI/navBar/NavBar.tsx
@@ -1,11 +1,12 @@
 import argentBankLogo from "../../img/argentBankLogo.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faUserCircle } from "@fortawesome/free-solid-svg-icons"
+import { faUserCircle, faRightFromBracket, faCircleUser} from "@fortawesome/free-solid-svg-icons"
 import { NavLink } from "react-router"
 import { useAppSelector } from "../../app/hooks"
 
 const NavBar = () => {
   const isAuth = useAppSelector(state => state.auth.isAuthenticated)
+  const firstName = useAppSelector(state => state.profile.firstName)
   return (
     <nav className="main-nav">
       <NavLink to={{ pathname: "/" }} className="main-nav-logo">
@@ -18,14 +19,17 @@ const NavBar = () => {
       </NavLink>
       <div>
         {isAuth ? (
-          <NavLink to={{ pathname: "/logout" }} className="main-nav-item">
-            <FontAwesomeIcon icon={faUserCircle} />
-            Sign Out
+          <>
+          <NavLink to={{ pathname: "/profile" }} className="main-nav-item">
+            <FontAwesomeIcon icon={faCircleUser} /> {firstName}
           </NavLink>
+          <NavLink to={{ pathname: "/logout" }} className="main-nav-item">
+            <FontAwesomeIcon icon={faRightFromBracket} /> Sign Out
+          </NavLink>
+          </>
         ) : (
           <NavLink to={{ pathname: "login" }} className="main-nav-item">
-            <FontAwesomeIcon icon={faUserCircle} />
-            Sign In
+            <FontAwesomeIcon icon={faUserCircle} /> Sign In
           </NavLink>
         )}
       </div>
