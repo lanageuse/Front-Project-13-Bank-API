9ae08e5528b8caa172d9491e207b45ebc9ecf496
Dev Lanageuse <dev@lanageuse.com>
2025-09-05 12:32:29 +0200

    feat: ajout du composant ToastContainer pour les notifications dans MainLayout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,17 @@
 import { Outlet, useLocation } from "react-router"
 import NavBar from "../UI/navBar/NavBar"
 import Footer from "../UI/footer/Footer"
+import { Flip, ToastContainer } from "react-toastify"
 
 const MainLayout = () => {
-    let location = useLocation()
+  const location = useLocation()
   const isHome = location.pathname === '/'
   return (
     <>
       <NavBar />
       <main className={`main ${isHome ? '' : 'bg-dark' }`}>
         <Outlet />
+        <ToastContainer autoClose={2000} position="bottom-right" transition={Flip}/>
       </main>
       <Footer />
     </>
