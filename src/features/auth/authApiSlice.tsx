dc0c0c294c083b36691321cd3b3fa076b89fa72a
Dev Lanageuse <dev@lanageuse.com>
2025-09-05 12:03:23 +0200

    feat: ajout de authApiSlice pour la gestion de l'authentification

---
+++ src/features/auth/authApiSlice.tsx
@@ -0,0 +1,41 @@
+// Need to use the React-specific entry point to import `createApi`
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import type { RootState } from "../../app/store"
+
+type AuthApiResponse = {
+  body: {
+    token: string
+  }
+  message: string
+  status: number
+}
+
+export const authApiSlice = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3001/api/v1/user/",
+    prepareHeaders: (headers, { getState }) => {
+      const state = getState() as RootState
+      const token = state.auth.token
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`)
+      }
+      return headers
+    },
+  }),
+  reducerPath: "authApi",
+  // Tag types are used for caching and invalidation.
+  tagTypes: ["Auth"],
+  endpoints: build => ({
+    login: build.mutation<AuthApiResponse, { email: string; password: string }>(
+      {
+        query: credentials => ({
+          url: "/login",
+          method: "POST",
+          body: credentials,
+        }),
+      },
+    ),
+  }),
+})
+
+export const { useLoginMutation } = authApiSlice
