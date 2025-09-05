6feddf47414afab1a76831330f3e2227360a0806
Dev Lanageuse <dev@lanageuse.com>
2025-09-05 15:45:49 +0200

    feat: remplacement de Link par NavLink pour une navigation améliorée dans NavBar

--- src/UI/navBar/NavBar.tsx
+++ src/UI/navBar/NavBar.tsx
@@ -1,24 +1,33 @@
 import argentBankLogo from "../../img/argentBankLogo.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faUserCircle } from "@fortawesome/free-solid-svg-icons"
-import { Link } from "react-router"
+import { NavLink } from "react-router"
+import { useAppSelector } from "../../app/hooks"
 
 const NavBar = () => {
+  const isAuth = useAppSelector(state => state.auth.isAuthenticated)
   return (
     <nav className="main-nav">
-        <Link to={{pathname : '/'}} className="main-nav-logo" >
+      <NavLink to={{ pathname: "/" }} className="main-nav-logo">
         <img
           className="main-nav-logo-image"
           src={argentBankLogo}
           alt="Argent Bank Logo"
         />
         <h1 className="sr-only">Argent Bank</h1>
-      </Link>
+      </NavLink>
       <div>
-        <Link to={{pathname : 'login'}} className="main-nav-item">
-          <FontAwesomeIcon icon={faUserCircle} />
-          Sign In
-        </Link>
+        {isAuth ? (
+          <NavLink to={{ pathname: "/logout" }} className="main-nav-item">
+            <FontAwesomeIcon icon={faUserCircle} />
+            Sign Out
+          </NavLink>
+        ) : (
+          <NavLink to={{ pathname: "login" }} className="main-nav-item">
+            <FontAwesomeIcon icon={faUserCircle} />
+            Sign In
+          </NavLink>
+        )}
       </div>
     </nav>
   )
