cc69b88d4b2a7a5460fcf714e530bea25eeebb0f
Dev Lanageuse <dev@lanageuse.com>
2025-09-09 16:15:36 +0200

    feat(profile): Centralisation des constants et des régles de validation dans un seul fichier

---
+++ src/features/profile/constants.ts
@@ -0,0 +1,42 @@
+import type { ValidationRules, ProfileFormData, LoginFormData } from "./types";
+
+export const INITIAL_LOGIN_FORM_VALUE: LoginFormData = {
+  email: "",
+  password: "",
+  remember: false,
+} as const;
+
+export const INITIAL_PROFILE_FORM_VALUE: ProfileFormData = {
+  firstName: "",
+  lastName: "",
+} as const
+
+// Règles de validation
+export const VALIDATION_RULES: ValidationRules = {
+  firstName: {
+    minLength: 2,
+    maxLength: 30,
+    pattern: /^[a-zA-ZÀ-ÿ]+(?:['_\s-][a-zA-ZÀ-ÿ]+)*$/,
+  },
+  lastName: {
+    minLength: 2,
+    maxLength: 30,
+    pattern: /^[a-zA-ZÀ-ÿ]+(?:['_\s-][a-zA-ZÀ-ÿ]+)*$/,
+  },
+}  as const
+
+// Messages d'erreur
+export const ERROR_MESSAGES = {
+  firstName: {
+    required: 'Le prénom est requis',
+    minLength: `Le prénom doit contenir au moins ${VALIDATION_RULES.firstName.minLength} caractères`,
+    maxLength: `Le prénom ne peut pas dépasser ${VALIDATION_RULES.firstName.maxLength} caractères`,
+    pattern: 'Le prénom contient des caractères non autorisés',
+  },
+  lastName: {
+    required: 'Le nom est requis',
+    minLength: `Le nom doit contenir au moins ${VALIDATION_RULES.lastName.minLength} caractères`,
+    maxLength: `Le nom ne peut pas dépasser ${VALIDATION_RULES.lastName.maxLength} caractères`,
+    pattern: 'Le nom contient des caractères non autorisés',
+  },
+} as const
