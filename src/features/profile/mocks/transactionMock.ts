011e6af9df3c450201d70a790d70e15e3bfa579d
Dev Lanageuse <dev@lanageuse.com>
2025-09-09 16:53:19 +0200

    feat(profile): Ajout des données mockés pour tester

---
+++ src/features/profile/mocks/transactionMock.ts
@@ -0,0 +1,39 @@
+import type { TransactionProps } from "../types"
+
+export const transactionMock: TransactionProps[] = [
+  {
+    title: "Argent Bank Checking (x8349)",
+    amount: 2082.79,
+    description: "Available Balance",
+  },
+  {
+    title: "Argent Bank Checking (x8349)",
+    amount: 2082.79,
+    description: "Available Balance",
+  },
+  {
+    title: "Argent Bank Checking (x8349)",
+    amount: 2082.79,
+    description: "Available Balance",
+  },
+  {
+    title: "Argent Bank Checking (x8349)",
+    amount: 2082.79,
+    description: "Available Balance",
+  },
+  {
+    title: "Argent Bank Checking (x8349)",
+    amount: 2082.79,
+    description: "Available Balance",
+  },
+  {
+    title: "Argent Bank Checking (x8349)",
+    amount: 2082.79,
+    description: "Available Balance",
+  },
+  {
+    title: "Argent Bank Checking (x8349)",
+    amount: 2082.79,
+    description: "Available Balance",
+  },
+]
