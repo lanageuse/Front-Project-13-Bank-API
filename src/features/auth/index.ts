29a8b519b977171c76b648ec77550f9112320dc5
Dev Lanageuse <dev@lanageuse.com>
2025-09-10 12:24:10 +0200

    feat(auth): Centralisation de l'export des modules

---
+++ src/features/auth/index.ts
@@ -0,0 +1,24 @@
+// ===== COMPONENTS EXPORTS =====
+export * from './components'
+
+// ===== HOOKS EXPORTS =====
+export * from './hooks'
+
+// ===== REDUX EXPORTS =====
+export { setAuth, setToken, logout } from '../auth/authSlice'
+export type { AuthState } from './types'
+
+// ===== SERVICES EXPORTS =====
+export * from './services'
+
+// ===== TYPES EXPORTS =====
+export * from './types'
+
+// ===== CONSTANTS EXPORTS =====
+export * from './constants'
+
+// ===== UTILS EXPORTS =====
+export * from './utils'
+
+// ===== CONSTANTS EXPORTS =====
+export * from './constants'
