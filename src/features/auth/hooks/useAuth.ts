82d818c39f446c516b09436a61cda1e8a2473a59
Dev Lanageuse <dev@lanageuse.com>
2025-09-15 17:47:55 +0200

    refact: remplacement de la function handler loginSucess par l'action asynchrone LoginUser

--- src/features/auth/hooks/useAuth.ts
+++ src/features/auth/hooks/useAuth.ts
@@ -1,11 +1,10 @@
 import { toast } from "react-toastify"
-import type { AppDispatch } from "../../../app/store"
-import { setAuth, setToken } from "../authSlice"
+import { loginUser } from "../authSlice"
 import type { LoginFormData } from "../types"
 import { INITIAL_LOGIN_FORM_VALUE } from "../constants"
 import { useState } from "react"
-import { useLoginMutation, validateAuth } from "../services"
-import { useAppDispatch } from "../../../app/hooks"
+import { validateAuth } from "../services"
+import { useAppDispatch, useAppSelector } from "../../../app/hooks"
 import { useNavigate } from "react-router"
 
 /**
@@ -20,9 +19,11 @@ export const useAuth = () => {
     INITIAL_LOGIN_FORM_VALUE,
   )
   const { email, password, remember } = formValue
-  const [login, { isLoading }] = useLoginMutation()
   const dispatch = useAppDispatch()
   const navigate = useNavigate()
+
+  const status = useAppSelector(state => state.auth.status)
+  const isLoading = status === "loading"
 
   /**
    * Gestionnaire de changement des champs du formulaire
@@ -46,58 +47,22 @@ export const useAuth = () => {
     e.preventDefault()
     try {
       // Validation des données du formulaire
-      const {isValid, errors} = validateAuth({email, password})
-      if(!isValid){
-        toast.warn(
-          errors.join('\n')
-        )
+      const { isValid, errors } = validateAuth({ email, password })
+      if (!isValid) {
+        toast.warn(errors.join("\n"))
         return
       }
-      
       // Tentative de connexion
-      const result = await login({ email, password }).unwrap()
-      const token = result.body.token
-      dispatch(handleLoginSucess(token, remember))
+      await dispatch(loginUser({ email, password, remember }))
       setFormValue(INITIAL_LOGIN_FORM_VALUE)
-      void navigate("/profile")
+      await navigate("/profile")
     } catch (error) {
       console.warn(error)
     }
   }
-  
-  /**
-   * Gestionnaire de succès de connexion
-   * @param {string} token - Token JWT reçu
-   * @param {boolean} remember - Indique si l'utilisateur veut rester connecté
-   * @returns {Function} Thunk Redux pour gérer l'état d'authentification
-   */
-  const handleLoginSucess =
-    (token: string, remember: boolean) => (dispatch: AppDispatch) => {
-      dispatch(setToken(token))
-      dispatch(setAuth(true))
-      
-      // Gestion & nettoyage du type de storage utilisé en function des choix utilisateur   
-      if (remember) {
-        localStorage.setItem("token", token)
-        localStorage.setItem("auth", "true")
-        localStorage.setItem("rememberMe", "true")
-
-        sessionStorage.removeItem("token")
-        sessionStorage.removeItem("auth")
-      } else {
-        sessionStorage.setItem("token", token)
-        sessionStorage.setItem("auth", "true")
-
-        localStorage.removeItem("token")
-        localStorage.removeItem("auth")
-        localStorage.removeItem("rememberMe")
-      }
-      toast.success("Login sucess")
-    }
 
   return {
     formValue,
-    handleLoginSucess,
     handleChange,
     handleSubmit,
     isLoading
