40115126b4ab65f1394ca225e4fdd2a5189e4200
Dev Lanageuse <dev@lanageuse.com>
2025-11-27 17:34:41 +0100

    fix (auth) : supression de l'action inutlisé setStatus

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -2,7 +2,6 @@ import type { PayloadAction } from "@reduxjs/toolkit"
 import { createAppSlice } from "../../app/createAppSlice"
 import { getStoredAuthData } from "./utils/utils"
 import type { AuthState } from "./types"
-import { STATE } from "./constants"
 
 // Récupération de l'état initial depuis le localStorage/sessionStorage à l'aide de la function utilitaire getStoredAuthData
 const initialState: AuthState = getStoredAuthData()
@@ -20,9 +19,6 @@ export const authSlice = createAppSlice({
     // Stocke le token JWT
     setToken: create.reducer((state, action: PayloadAction<string>) => {
       state.token = action.payload
-    }),
-    setStatus : create.reducer((state, action : PayloadAction<typeof STATE[keyof typeof STATE]>)=>{
-      state.status = action.payload
     }),
     // Déconnexion : réinitialise l'état et nettoie le stockage
     logout: create.reducer(state => {
