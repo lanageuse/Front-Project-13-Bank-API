33ae87e67200fb2bedb08d1894b2a93739310a47
Dev Lanageuse <dev@lanageuse.com>
2025-12-01 13:47:05 +0100

    chore(useProfile) : supression de l'état local de la gestion des erreurs : Douclons avec le middle rtkQueryErrorLogger

--- src/features/profile/hooks/useProfile.ts
+++ src/features/profile/hooks/useProfile.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { useAppDispatch, useAppSelector } from "../../../app/hooks"
 import { setProfile } from "../profileSlice"
 import { useProfileMutation } from "../services/profileApi"
@@ -13,17 +13,13 @@ export const useProfile = () => {
   const [fetchProfile, {isLoading}] = useProfileMutation()
   const user: ProfileState = useAppSelector(state => state.profile)
   const dispatch = useAppDispatch()
-  const [error, setError] = useState<string | null>(null)
 
   useEffect(() => {
     const fetchUserProfile = async () => {
       try {
-        setError(null)
         const result = await fetchProfile(undefined).unwrap()
         dispatch(setProfile(result.body))
       } catch (error) {
-        const errorMessage = error instanceof Error ? error.message : 'Une erreur est survenue'
-        setError(errorMessage)
         console.error('Erreur lors de la récupération du profil:', error)
       }
     }
@@ -35,7 +31,6 @@ export const useProfile = () => {
   return {
     user,
     isLoading,
-    error,
     refetch: () => fetchProfile(undefined)
   }
 }
